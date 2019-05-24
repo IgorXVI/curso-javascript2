b8047fffebf3605863992e9bd47c92dcf6d37980
Igor Almeida <inazumaseleven04@gmail.com>
2019-05-24 17:07:40 -0300

    aula 3 e 4

---
+++ client/js/app/NegociacaoController.js
@@ -0,0 +1,40 @@
+class NegociacaoController {
+
+    constructor() {
+
+        let $ = document.querySelector.bind(document);
+
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
+
+    }
+
+    adiciona(event) {
+        event.preventDefault();
+
+        this._listaNegociacoes.adiciona(_this.criaNegociacao());
+        this._limpaFormulario();
+    }
+
+    _criaNegociacao(){
+        let helper = new DateHelper();
+
+        return new Negociacao(
+            helper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+    }
+
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0
+
+        this._inputData.focus();
+
+    }
+    
+}
