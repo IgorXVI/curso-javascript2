d555a509ca5dec44a23b7ee96143c923e6c4f07a
Igor Almeida <inazumaseleven04@gmail.com>
2019-05-30 17:26:22 -0300

    terminei a parte I e estou na parte II aula 2 atividade 10

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,29 +1,30 @@
 class Negociacao {
-
+    
     constructor(data, quantidade, valor) {
-
+        
         this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
         Object.freeze(this);
-
     }
-
+    
     get volume() {
-
+        
         return this._quantidade * this._valor;
-    
-    }
-
-    getData() {
-        return this._data;
     }
     
-    getQuantidade() {
+    get data() {
+        
+        return new Date(this._data.getTime());
+    }
+    
+    get quantidade() {
+        
         return this._quantidade;
     }
     
-    getValor() {
+    get valor() {
+        
         return this._valor;
     }
 }
